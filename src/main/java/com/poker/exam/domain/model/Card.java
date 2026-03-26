03c6e28fef256b0e4d8df43e1c33a7b299ebecd2
BXSoft <jeremiahlowinne@gmail.com>
2026-03-26 14:47:46 +0100

    clean comment

--- src/main/java/com/poker/exam/domain/model/Card.java
+++ src/main/java/com/poker/exam/domain/model/Card.java
@@ -2,7 +2,6 @@ package com.poker.exam.domain.model;
 
 public record Card(Rank rank, Suit suit) {
 
-    // Surcharge optionnelle mais très pratique pour lire tes logs d'erreurs de tests !
     @Override
     public String toString() {
         return rank.name() + " of " + suit.name();
--- src/test/java/com/poker/exam/domain/model/CardTest.java
+++ src/test/java/com/poker/exam/domain/model/CardTest.java
@@ -7,21 +7,17 @@ class CardTest {
 
     @Test
     void cardsWithSameRankAndSuitShouldBeEqual() {
-        // Étant donné (Arrange)
         Card card1 = new Card(Rank.ACE, Suit.SPADES);
         Card card2 = new Card(Rank.ACE, Suit.SPADES);
 
-        // Alors (Assert) - Vérifie que le record gère bien l'égalité par valeur
         assertThat(card1).isEqualTo(card2);
     }
 
     @Test
     void aceShouldHaveHigherValueThanKing() {
-        // Étant donné
         Card ace = new Card(Rank.ACE, Suit.HEARTS);
         Card king = new Card(Rank.KING, Suit.SPADES);
 
-        // Alors
         assertThat(ace.rank().getValue()).isGreaterThan(king.rank().getValue());
     }
 }
